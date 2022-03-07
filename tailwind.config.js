69d962f4968702f15dd948684b3a75568add4b13
Alexander Thoren <thoren.alex@gmail.com>
2022-03-07 11:03:28 +0100

    :lipstick: Added tailwind screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,22 @@ module.exports = {
       fontFamily: {
         menlo: ["Menlo", "Monaco", "Lucida Console", "Liberation Mono", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Courier New", "monospace"],
       },
+      screens: {
+        '2xl-max': { 'max': '1535px' },
+        // => @media (max-width: 1535px) { ... }
+
+        'xl-max': { 'max': '1279px' },
+        // => @media (max-width: 1279px) { ... }
+
+        'lg-max': { 'max': '1023px' },
+        // => @media (max-width: 1023px) { ... }
+
+        'md-max': { 'max': '767px' },
+        // => @media (max-width: 767px) { ... }
+
+        'sm-max': { 'max': '639px' },
+        // => @media (max-width: 639px) { ... }
+      }
     },
   },
   plugins: [],
