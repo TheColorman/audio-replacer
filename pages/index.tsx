293e3b16f38fddd3019efd3f26c09b5fb60d08d2
Alexander Thoren <thoren.alex@gmail.com>
2022-03-15 18:48:01 +0100

    :construction: Added step three

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,17 @@ const Home: NextPage = () => {
   const handleBack = () => {
     setActiveStep(prevActiveStep => prevActiveStep - 1)
   }
+  const handleReset = () => {
+    setActiveStep(0)
+  }
+
+  const uploadVideoToClient = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      const i = e.target.files[0]
+
+      setVideo(i)
+    }
+  }
 
   const Progress = () => {
     return (
@@ -228,6 +239,86 @@ const Home: NextPage = () => {
       </>
     )
   }
+  const StepThree = () => {
+    const [progress, setProgress] = React.useState(100)
+
+    React.useEffect(() => {
+      const timer = setInterval(() => {
+        setProgress((oldProgress) => {
+          if (oldProgress === 100) {
+            return 100
+          }
+          const diff = Math.random() * 10
+          return Math.min(oldProgress + diff, 100)
+        })
+      }, 500)
+
+      return () => {
+        clearInterval(timer)
+      }
+    }, [])
+
+    if (progress < 100) {
+      return (
+        <>
+          <p className={styles.description}>
+            Creating your video...
+          </p>
+
+          <div className='w-1/2'>
+            <NoSsr>
+              <LinearProgress variant='determinate' value={progress} />
+            </NoSsr>
+          </div>
+        </>
+      )
+    }
+    return (
+      <>
+        <p className={styles.description}>
+          Your video is ready!
+        </p>
+        <div className="flex flex-col items-center">
+          <div className="bg-gray-600 w-96 aspect-video relative">
+            <div className="w-full h-8 bg-black bottom-0 absolute">
+              <p className="text-sm text-white p-1">
+                Video name
+              </p>
+            </div>
+          </div>
+          <div className="flex mt-4 w-full space-x-4">
+            <button className={`${styles.buttonSecondary} w-full`}>
+              <span className="mr-1">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
+                </svg>
+              </span>
+              Copy URL
+            </button>
+            <button className={`${styles.buttonPrimary} w-full`}>
+              <span className="mr-1">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+                </svg>
+              </span>
+              Download video
+            </button>
+          </div>
+          <div className="flex mt-12 w-full">
+            <button className={`${styles.buttonPrimary} w-full`} onClick={handleReset}>
+              Create a new video
+            </button>
+          </div>
+        </div>
+      </>
+    )
+  }
+
+  const steps = [
+    <StepOne key={0} />,
+    <StepTwo key={1} />,
+    <StepThree key={2} />,
+  ]
 
 
   return (
