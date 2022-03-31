fda8c13eaf9ce7cb36056dd01c061a386e992e5c
Alexander Thoren <thoren.alex@gmail.com>
2022-03-31 12:01:28 +0200

    :lipstick: Extended clickable area to entire box

--- pages/index.tsx
+++ pages/index.tsx
@@ -216,11 +216,11 @@ const Home: NextPage = () => {
 
         <div className="flex flex-col">
           <div className="flex flex-row">
-            <div className={styles.buttonPrimary}>
-              <p className="absolute left-1/2 -translate-x-1/2 -top-5 text-sm text-black align-text-bottom w-screen">
-                {audio ? audio.name : ''}
-              </p>
-              <label htmlFor="upload-video" className='hover:cursor-pointer inline-flex'>
+            <div className='relative'>
+              <label htmlFor="upload-video" className={`${styles.buttonPrimary} hover:cursor-pointer inline-flex h-full`}>
+                <p className='absolute left-1/2 -translate-x-1/2 -top-5 text-sm text-black align-text-bottom w-screen'>
+                  {audio ? audio.name : ''}
+                </p>
                 <span className="mr-1">
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
