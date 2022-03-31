e1c60dcc725f1239c61a88f638733d6c793d0ff1
Alexander Thoren <thoren.alex@gmail.com>
2022-03-31 11:00:04 +0200

    :+1: Update

---
+++ _next/static/dCvl_ZlbbqGQ6RLjB6yRk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/350-beb02b9563ae548a.js","static/css/4c5c17ad257cffea.css","static/chunks/pages/index-5d0d60689d0e04e2.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
