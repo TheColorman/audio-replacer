65ba7d8aa7eba2143b984b5a17b9fb0b0b03d27a
Alexander Thoren <thoren.alex@gmail.com>
2022-03-31 10:57:31 +0200

    :sparkles: Addded service worker to intercept request headers
    See https://dev.to/josedonato/adding-a-service-worker-into-your-next-js-application-1dib

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,33 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      const registerWorker = () => {
+        // Register service worker
+        navigator.serviceWorker.register("/sw.js").then(
+          (registration) => {
+            console.log("COOP/COEP Service Worker registered", registration.scope)
+            // If the registration is active, but it's not controlling the page
+            if (registration.active && !navigator.serviceWorker.controller) {
+              window.location.reload()
+            }
+          },
+          function (err) {
+            console.log("COOP/COEP Service Worker failed to register", err)
+          }
+        )
+      }
+      window.addEventListener("load", registerWorker)
+      return function cleanupListener() {
+        window.removeEventListener("load", registerWorker)
+      }
+    } else {
+      console.warn("Cannot register a service worker")
+    }
+  })
   return <Component {...pageProps} />
 }
 
