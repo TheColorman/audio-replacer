37010960c5ce79e70ec2f8a9f5848463f96a9a10
Alexander Thoren <thoren.alex@gmail.com>
2022-04-28 20:21:29 +0200

    :children_crossing: Updated button text

--- pages/index.tsx
+++ pages/index.tsx
@@ -123,7 +123,7 @@ const Home: NextPage = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
                   </svg>
                 </span>
-                Upload video
+                Upload video/image
               </label>
               <input
                 // Hide the file input, but remain accessible to screen readers and keyboard users
